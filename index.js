5ef744022a3c73ff320bb0c8c73978b85f0a70f0
Letícia Ribeiro <lerihribeiro@gmail.com>
2024-09-23 19:34:13 -0300

    testando todas as funcionalidades

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const primeiroNumero = 0;
-const segundoNumero = 0;
+const primeiroNumero = 20;
+const segundoNumero = 10;
 
 function somar(num1, num2){
     return num1 + num2;
@@ -16,3 +16,8 @@ function multiplicar(num1, num2) {
 function dividir(num1, num2) {
     return num1 / num2;
 }
+
+console.log(somar(primeiroNumero, segundoNumero));
+console.log(subtrair(primeiroNumero, segundoNumero));
+console.log(multiplicar(primeiroNumero, segundoNumero));
+console.log(dividir(primeiroNumero, segundoNumero));
